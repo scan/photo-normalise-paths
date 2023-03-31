cc4a43e4ed24a29ed39e58cb972d64e2b4567ee5
Tom Streller <tom@streller.me>
2023-03-31 10:34:28 +0200

    first commit

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+#[tokio::main]
+pub async fn main() -> anyhow::Result<()> {
+    let subscriber = tracing_subscriber::FmtSubscriber::new();
+    tracing::subscriber::set_global_default(subscriber)?;
+
+    Ok(())
+}
